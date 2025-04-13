ad40bd1dcc58c6fbbf15278a7b6698b040c59ba5
QSupratik <supratikdatta2k20@gmail.com>
2025-04-13 16:03:00 +0530

    Basic Hero section structure created

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,65 +1,80 @@
 import styles from "./Hero.module.css";
 import SpeechBubble from "./SpeechBubble.jsx";
+import {motion} from "motion/react";
 
-import award1 from "../Images/award1.png";
-import award2 from "../Images/award2.png";
-import award3 from "../Images/award3.png";
 import insta from "../Images/instagram.png";
+import facebook from "../Images/facebook.png";
+import linkedin from "../Images/linkedin.png";
+import hackerrank from "../Images/hackerrank.png";
+import me from "../Images/me.png";
 
 function Hero() {
     return (
         <div className={styles.hero}>
-            <div className="hero-left">
+            
+            {/* Left Side of Hero Section */}
+            <div className={styles.hero_left}>
 
-                <h1 className="title">
+                {/* Heading Section */}
+                <motion.h1 className={styles.title}>
                     Hi There!
-                    <br />
-                    <span className="name">I am Robert</span>
-                </h1>
-                <h2>Top-Rated Designer</h2>
-                <div className="award-list">
-                    <img src={award1} alt="Awaed1" />
-                    <img src={award2} alt="Awaed1" />
-                    <img src={award3} alt="Awaed1" />
+                     <br />
+                    <span className={styles.name}>I am Supratik</span>
+                </motion.h1>
+
+                {/* Mid Region Text descibing Position */}
+                <div className={styles.mid_text}>
+                    <h2>Full Stack Developer</h2>
+                    <h4>MERN Stack</h4>
                 </div>
 
+                {/* Certificate */}
+                <div className={styles.certificate}>
+                    <p align="center"><img src={hackerrank} align="center" alt="hackerrank_logo" className={styles.hackerrank} /></p>
+                    <p align="center">HackerRank certified</p>
+                    <br></br>
+                    <p align="center">Software Engineer Intern</p>
+                </div>
             </div>
 
-            <div className="hero-right">
+            {/* Background Features */}
+            <div className="heroBackground">
+                <p align="center" className={styles.hImg}><img src={me} alt="My image"/>
+                </p>
+            </div>
+
+            {/* Right Side of Hero Section */}
+            <div className={styles.hero_right}>
 
                 {/*Follow Part*/}
-                <div className="follow">
+                <div className={styles.follow}>
                     <a href="/">
-                        <img src={insta}></img>
+                        <img src={insta} className={styles.instagramFollowIcons}></img>
                     </a>
-                    {/* <a href="/">
-                        <image src={github}></image>
-                    </a> */}
-                    {/* <a href="/">
-                        <image src={linkedin}></image>
-                    </a> */}
+                    <a href="/">
+                        <img src={facebook} className={styles.facebookFollowIcons}></img>
+                    </a>
+                    <a href="/">
+                        <img src={linkedin} className={styles.linkedinFollowIcons}></img>
+                    </a>
                 </div>
 
                 {/* Speech Bubble */}
                 <SpeechBubble></SpeechBubble>
-                <div className="certificate">
-                    djsnsc
-                    <br></br>
-                </div>
 
-                <div className="contact-buttons">
+                {/* Contact Me */}
+                <div className={styles.contact_button}>
                     <a href="/#contact">
                         <svg width="100" height="100" viewBox="0 0 100 100">
                             <circle cx="50" cy="50" r="40" fill="pink" />
-                            <text x="52" y="55" textAnchor="middle" fill="black" fontSize="16" fontFamily="Arial">
+                            <text x="52" y="55" textAnchor="middle" fill="black" fontSize="16" fontFamily="Arial" className={styles.circleText}>
                                 Contact Me!
                             </text>
                         </svg>
-
-
                     </a>
                 </div>
             </div>
+
         </div>
     )
 }
--- src/Components/SpeechBubble.jsx
+++ src/Components/SpeechBubble.jsx
@@ -1,7 +1,30 @@
-function SpeechBubble(){
-    return(
-        <div className="speech-bubble">
-            
+import styles from "./SpeechBubble.module.css";
+import man from "../Images/man.png";
+import { TypeAnimation } from 'react-type-animation';
+
+function SpeechBubble() {
+    return (
+        <div className={styles.bubbleContainer}>
+            <div className={styles.bubble}>
+                <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'We produce food for Mice',
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        'We produce food for Hamsters',
+                        1000,
+                        'We produce food for Guinea Pigs',
+                        1000,
+                        'We produce food for Chinchillas',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={40}
+                    style={{ fontSize: '2em', display: 'inline-block' }}
+                    repeat={Infinity}
+                />
+            </div>
+            <img src={man} alt="man.jpg" className={styles.manImage}></img>
         </div>
     );
 }
